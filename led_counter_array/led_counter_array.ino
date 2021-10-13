5fa547946500749757271b555a31ae662d54c65c
Kenny <kennyd123456@hotmail.com>
2021-10-13 18:32:36 -0400

    fixed name for counter LED array

---
+++ led_counter_array/led_counter_array.ino
@@ -0,0 +1,26 @@
+const int led[10]=[2,3,4,5,6,7,8,9,10,11];
+int x=0;
+
+void setup() {
+  for (int i : led){  
+    pinMode(i, OUTPUT);
+  }
+  
+  pinMode(13, INPUT);
+}
+
+void loop() {
+    if(digitalRead(13)==HIGH){
+    digitalWrite(led[x],HIGH);
+    x++;
+    if (x==12){
+      x=0;
+    }
+    delay (300);
+}
+
+
+
+
+
+}
