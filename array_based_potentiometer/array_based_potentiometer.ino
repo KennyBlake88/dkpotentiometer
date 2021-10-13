cc285b000dc7ee5bcc667af2c58863660dae3eac
Kenny <kennyd123456@hotmail.com>
2021-10-12 23:41:42 -0400

    Documented the code

--- array_based_potentiometer/array_based_potentiometer.ino
+++ array_based_potentiometer/array_based_potentiometer.ino
@@ -1,11 +1,14 @@
 /*
  * Array Based Potentiomic LED Array 
  * Credit: Kenny Blake
- * Version: 0.0.1
+ * Version: 1.0.0
  */
 
 const int LEDArray [6] = {8, 9, 10, 11, 12, 13};
 
+/**
+ * Sets all the arrays the be on, begins the serial reading. 
+ */
 void setup() {
   for (int i = 0; i < sizeof(LEDArray); i++)
   {
@@ -15,6 +18,12 @@ void setup() {
 
 }
 
+/**
+ * Changes the state of the led's in the array, up to index upToWhat, to HIGH if state is true, and low if state is false, and changes the rest of them to be LOW.
+ * @param upToWhat (int):   The index of the LED in the array. (For example, 0 -> 8, 1 -> 9, 2 -> 10)
+ * @param state    (bool):  Whether we want to have them change to on or off. true for on, false for off. 
+ * 
+ */
 void changeState(int upToWhat, bool state)
 {
     for (int i = 0; i < upToWhat; i++)
@@ -35,6 +44,9 @@ void changeState(int upToWhat, bool state)
     }
 }
 
+/**
+ * Reads data from analog port 0 from the potentiometer, and calls the changeState function based on the passed data.
+ */
 void loop() {
   int x=(analogRead(A0));
   Serial.println(x);
